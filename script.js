e068e1f12659bb6d5a8fae207254591ca0b0fee2
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2024-10-24 18:34:23 -0700

    Add CSS rules/Fix Images

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+
+
+const theSiberian = document.getElementById("theSiberian");
+
+function changeColor(event) {
+   event.target.style.backgroundColor = "green";
+   event.target.textContent = "I love Siberian Cats 😻"
+}
+
+theSiberian.addEventListener("click", changeColor);
+
+
+function colorBack(event){
+   event.target.style.backgroundColor = "red";
+   event.target.textContent = "Click Me 😾"
+}
+
+theSiberian.addEventListener("mouseout", colorBack);
